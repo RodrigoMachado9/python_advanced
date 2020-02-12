bd3481ac3e572b4584cfee6120006562cdb16d71
RodrigoMachado9 <rodrigo.machado@truckpad.com.br>
2020-02-12 00:47:53 -0300

    in progress

---
+++ linked_list/deletion_in_single_linkedlist.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+        print("the currente value upgrade: %s " % self.data)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printing(self):
+        print_value = self.head
+        while print_value is not None:
+            print(print_value.data)
+            print_value = print_value.next
+
+
+
+x = LinkedList()
+x.head = Node("rmachado")
+data2 = Node("truck")
+data3 = Node("driver")
+data4 = Node("shipper")
+
+# todo, multiple instances : : beautifull . . . .
+x.head.next = data2
+data2.next = data3
+data3.next = data4
+
+
+x.printing()
